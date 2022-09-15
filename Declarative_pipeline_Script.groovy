5cab4aa0e4d197649150d833e03d88054fd93e1d
Hack-with-8k0b <nbharathkumara@gmail.com>
2022-09-15 18:16:29 +0530

    test

--- Declarative_pipeline_Script.groovy
+++ Declarative_pipeline_Script.groovy
@@ -1,6 +1,6 @@
 def dockerrun = 'docker run -p 8000:80 -d --name bkob-server nbharathkumara/$JOB_NAME:latest'
-def dockerrm = 'docker container rm -f bkob-server &>/dev/null '
-def dockerimagerm = 'docker image rmi nbharathkumara/$JOB_NAME &>/dev/null '
+def dockerrm = 'docker container rm -f bkob-server &>/dev/null'
+def dockerimagerm = 'docker image rmi nbharathkumara/$JOB_NAME &>/dev/null'
 
 //def dockerrm = 'docker container rm -f bkob-server'
 //def dockerimagerm = 'docker image rmi nbharathkumara/$JOB_NAME'
