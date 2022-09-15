269fe36575df86e4842c0eb544dc25458a3534ca
Hack-with-8k0b <nbharathkumara@gmail.com>
2022-09-15 17:02:21 +0530

    pushed into docker hub

--- Declarative_pipeline_Script.groovy
+++ Declarative_pipeline_Script.groovy
@@ -16,5 +16,16 @@ pipeline{
                 sh 'docker image tag $JOB_NAME:v1.$BUILD_ID nbharathkumara/$JOB_NAME:latest'
             }
         }
+        stage('Pushing into DockerHub'){
+            steps{
+            withCredentials([string(credentialsId: 'dockerpass', variable: 'dockerpassword')]) {
+            sh 'docker login -u nbharathkumara -p ${dockerpassword}'
+            sh 'docker image push nbharathkumara/$JOB_NAME:v1.$BUILD_ID'
+            sh 'docker image push nbharathkumara/$JOB_NAME:latest'
+            sh 'docker image rmi $JOB_NAME:v1.$BUILD_ID nbharathkumara/$JOB_NAME:v1.$BUILD_ID nbharathkumara/$JOB_NAME:latest '
+
+            }
+
+        }
     }
 }
